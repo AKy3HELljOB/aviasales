5c4536c212db67423faaa587f94aae0d075b96c0
AKy3HELljOB <bombber@bk.ru>
2020-12-09 00:48:42 +0400

    add spaicing for price

--- src/components/Ticket/Header/Header.jsx
+++ src/components/Ticket/Header/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Header.styles.css';
 
+const formatPrice = (x) => {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")  + " Р";
+}
+
 const Header = (props) => (
   <div className="headerWrapper">
-    <div className="price">{props.price}</div>
+    <div className="price">{formatPrice(props.price)}</div>
     <span></span>
     <img className="logo" src={props.logo}></img>
   </div>
